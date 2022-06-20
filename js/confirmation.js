5ef5666f063f36308366b1f44364039ce1eb3d00
GulcanC <gulcancoskun.a@gmail.com>
2022-06-20 23:50:25 +0200

    Updates

---
+++ js/confirmation.js
@@ -0,0 +1,11 @@
+
+
+
+function validation(){
+    const getOrderId = document.getElementById("orderId");
+    getOrderId.innerText = localStorage.getItem("orderId");
+    console.log(localStorage.getItem("orderId"))
+   // localStorage.clear();
+}
+
+validation();
